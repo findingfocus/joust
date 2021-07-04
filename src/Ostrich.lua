d71366ffb917bb499443d1ea43108e523b7852da
yakulbeansMacLaptop <yakulbeans@gmail.com>
2021-07-03 21:53:51 -0600

    changed PLAYER SPEED and refactored player controls out of the Ostrich function

--- src/Ostrich.lua
+++ src/Ostrich.lua
@@ -5,23 +5,34 @@ function Ostrich:init(x, y)
 	self.y = y
 	self.width = 100
 	self.dy = 0
+	self.dx = 0 
+	local playerSpeed = 0
 end
 
 function Ostrich:update(dt)
 	self.dy = self.dy + GRAVITY * dt
 	self.y = math.min(VIRTUAL_HEIGHT - self.width - 50, self.dy + self.y)
 
-	if love.keyboard.isDown('right') then
+	--[[
+if love.keyboard.isDown('right') then
 		self.x = (self.x + PLAYER_SPEED * dt) % VIRTUAL_WIDTH
+		self.dx = 6
 	end
+
+---[[
+	repeat
+		PLAYER_SPEED = PLAYER_SPEED + self.dx
+	until (PLAYER_SPEED >= 1800)
 
 	if love.keyboard.isDown('left') then
 		self.x = (self.x - PLAYER_SPEED * dt) % VIRTUAL_WIDTH
+		PLAYER_SPEED = self.dx + 300
 	end
 
 	if love.keyboard.wasPressed('space') and grounded then
 		self.dy = -30
 	end
+	--]]
 end
 
 function Ostrich:render()
@@ -31,10 +42,4 @@ function Ostrich:render()
 
 	love.graphics.setColor(255/255, 255/255, 255/255, 255/255)
 
-	love.graphics.print(table.concat({
-		' '..'  ',
-		' '..'  ',
-		'playerX: '..math.floor(self.x),
-		'playerY: '..math.floor(self.y),
-	}, '\n'))
 end
--- states/PlayState.lua
+++ states/PlayState.lua
@@ -2,8 +2,8 @@ PlayState = Class{__includes = BaseState}
 
 function PlayState:init()
 	player1 = Ostrich(VIRTUAL_WIDTH / 2 - 50, 500)
-	PLAYER_SPEED = 500
 	GRAVITY = 120
+	playerSpeed = 500
 end
 
 function PlayState:update(dt)
@@ -14,12 +14,33 @@ function PlayState:update(dt)
 		gStateMachine:change('helpState')
 	end
 
+	if love.keyboard.isDown('right') then
+		player1.x = (player1.x + playerSpeed * dt) % VIRTUAL_WIDTH
+		self.dx = 6
+	end
+
+--[[
+	repeat
+		PLAYER_SPEED = PLAYER_SPEED + self.dx
+	until (PLAYER_SPEED >= 1800)
+--]]
+	if love.keyboard.isDown('left') then
+		player1.x = (player1.x - playerSpeed * dt) % VIRTUAL_WIDTH
+		player1.dx = 6
+	end
+
+	if love.keyboard.wasPressed('space') and grounded then
+		player1.dy = -30
+	end
+
 	if love.keyboard.wasPressed('r') then
 		--sounds['playMusic']:stop()
-		gStateMachine:change('titleState')
+		--gStateMachine:change('titleState')
 		playerX = 0
 		playerY = 800 - 110
 		playerDY = 0
+		PLAYER_SPEED = 500
+		player1.dx = 0
 	end
 
 
@@ -33,6 +54,15 @@ function PlayState:render()
 	love.graphics.rectangle('fill', 0, VIRTUAL_HEIGHT - 50, VIRTUAL_WIDTH, 50)
 
 	player1:render()
+
+	love.graphics.print(table.concat({
+		'',
+		'',
+		'playerX: '..math.floor(player1.x),
+		'playerY: '..math.floor(player1.y),
+		'player.dx: '..math.floor(player1.dx),
+		'PLAYER_SPEED: '..math.floor(PLAYER_SPEED),
+	}, '\n'))
 	--love.graphics.setColor(255/255, 255/255, 255/255, 255/255)
 	--love.graphics.printf('Hello PlayState', 0, 200, VIRTUAL_HEIGHT / 2, 'center')
 end 
