698abd58d3de18855484fb600627ddec9ecefe1e
yakulbeansMacLaptop <yakulbeans@gmail.com>
2023-10-28 00:20:42 -0600

    added third input initial for high score state

--- states/HighScoreState.lua
+++ states/HighScoreState.lua
@@ -10,8 +10,11 @@ function HighScoreState:init()
     letter1InputChoice = true
     letter2InputChoice = false
     letter3InputChoice = false
+    scoreLocked = false
     letter1Locked = 'A'
     flashTimer = .5
+    sounds['beep']:setVolume(0.3)
+    sounds['select']:setVolume(0.3)
 end
 
 function HighScoreState:update(dt)
@@ -22,6 +25,7 @@ function HighScoreState:update(dt)
    end
    if letter1InputChoice then
        if love.keyboard.wasPressed('down') then
+           sounds['beep']:play()
            flashing = false
            flashTimer = .5
            letter1Index = letter1Index + 1
@@ -30,6 +34,7 @@ function HighScoreState:update(dt)
            end
        end
        if love.keyboard.wasPressed('up') then
+           sounds['beep']:play()
            flashing = false
            flashTimer = .5
            letter1Index = letter1Index - 1
@@ -39,6 +44,7 @@ function HighScoreState:update(dt)
        end
     elseif letter2InputChoice then
        if love.keyboard.wasPressed('down') then
+           sounds['beep']:play()
            flashing = false
            flashTimer = .5
            letter2Index = letter2Index + 1
@@ -47,6 +53,7 @@ function HighScoreState:update(dt)
            end
        end
        if love.keyboard.wasPressed('up') then
+           sounds['beep']:play()
            flashing = false
            flashTimer = .5
            letter2Index = letter2Index - 1
@@ -54,14 +61,44 @@ function HighScoreState:update(dt)
                letter2Index = 26
            end
        end
+    elseif letter3InputChoice then
+       if love.keyboard.wasPressed('down') then
+           sounds['beep']:play()
+           flashing = false
+           flashTimer = .5
+           letter3Index = letter3Index + 1
+           if letter3Index == 27 then
+               letter3Index = 1
+           end
+       end
+       if love.keyboard.wasPressed('up') then
+           sounds['beep']:play()
+           flashing = false
+           flashTimer = .5
+           letter3Index = letter3Index - 1
+           if letter3Index == 0 then
+               letter3Index = 26
+           end
+       end
     end
 
    if love.keyboard.wasPressed('right') or love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
-       if letter1InputChoice then
-            scoreInitials[1] = letters[letter1Index]
-            letter1InputChoice = false
-            letter2InputChoice = true
-       end
+       if not scoreLocked then
+           sounds['select']:play()
+           if letter1InputChoice then
+               scoreInitials[1] = letters[letter1Index]
+               letter1InputChoice = false
+               letter2InputChoice = true
+           elseif letter2InputChoice then
+               scoreInitials[2] = letters[letter2Index]
+               letter2InputChoice = false
+               letter3InputChoice = true
+           elseif letter3InputChoice then
+               scoreInitials[3] = letters[letter3Index]
+               letter3InputChoice = false
+               scoreLocked = true
+           end
+        end
    end
 end
 
@@ -89,5 +126,17 @@ function HighScoreState:render()
             love.graphics.setColor(255/255, 255/255, 255/255, 255/255)
         end
         love.graphics.printf(tostring(letters[letter2Index]), 8, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
+    elseif letter3InputChoice then
+        love.graphics.printf(tostring(scoreInitials[1]), 0, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
+        love.graphics.printf(tostring(scoreInitials[2]), 8, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
+        if flashing then
+            love.graphics.setColor(255/255, 255/255, 255/255, 0/255)
+        else
+            love.graphics.setColor(255/255, 255/255, 255/255, 255/255)
+        end
+        love.graphics.printf(tostring(letters[letter3Index]), 16, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
+    end
+    if scoreLocked then
+        love.graphics.printf('CONGRATULATIONS, ' .. scoreInitials[1] .. scoreInitials[2] .. scoreInitials[3] .. '!!!', 0, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
     end
 end
