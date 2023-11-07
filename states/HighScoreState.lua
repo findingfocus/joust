bd934fd7c5746ba29b184377c37df3128a66985f
yakulbeansMacLaptop <yakulbeans@gmail.com>
2023-11-06 22:25:40 -0700

    fixed flow of highscore input based whether player beats 10th high score

--- states/HighScoreState.lua
+++ states/HighScoreState.lua
@@ -6,8 +6,9 @@ function HighScoreState:init()
     letter1Index = 1
     letter2Index = 1
     letter3Index = 1
+    counter = 0
     flashing = false
-    letter1InputChoice = true
+    letter1InputChoice = false
     letter2InputChoice = false
     letter3InputChoice = false
     playerInitialsLocked = false
@@ -50,11 +51,16 @@ function HighScoreState:update(dt)
     saveDefaultScoreBoard()
     --loadHighScore()
     --CHECK IF PLAYER BEATS 10th PLACE SCOREBOARD
-    if Score > saveData[10].score then
+    if Score > saveData[10].score and counter == 0 then
         insertPlayerScore()
-        --playerInitialsLocked = false
-    else
-        --playerInitialsLocked = true
+        playerInitialsLocked = false
+        letter1InputChoice = true
+        counter = 1
+    elseif Score < saveData[10].score then
+        letter1InputChoice = false
+        letter2InputChoice = false
+        letter3InputChoice = false
+        playerInitialsLocked = true
     end
     --saveData[2].score = Score
     flashTimer = flashTimer - dt
@@ -62,61 +68,64 @@ function HighScoreState:update(dt)
         flashing = not flashing
         flashTimer = .5
     end
-    if letter1InputChoice then
-        if love.keyboard.wasPressed('down') then
-            sounds['beep']:play()
-            flashing = false
-            flashTimer = .5
-            letter1Index = letter1Index + 1
-            if letter1Index == 27 then
-                letter1Index = 1
+
+    if not playerInitialsLocked then
+        if letter1InputChoice then
+            if love.keyboard.wasPressed('down') then
+                sounds['beep']:play()
+                flashing = false
+                flashTimer = .5
+                letter1Index = letter1Index + 1
+                if letter1Index == 27 then
+                    letter1Index = 1
+                end
             end
-        end
-        if love.keyboard.wasPressed('up') then
-            sounds['beep']:play()
-            flashing = false
-            flashTimer = .5
-            letter1Index = letter1Index - 1
-            if letter1Index == 0 then
-                letter1Index = 26
+            if love.keyboard.wasPressed('up') then
+                sounds['beep']:play()
+                flashing = false
+                flashTimer = .5
+                letter1Index = letter1Index - 1
+                if letter1Index == 0 then
+                    letter1Index = 26
+                end
             end
-        end
-    elseif letter2InputChoice then
-        if love.keyboard.wasPressed('down') then
-            sounds['beep']:play()
-            flashing = false
-            flashTimer = .5
-            letter2Index = letter2Index + 1
-            if letter2Index == 27 then
-                letter2Index = 1
+        elseif letter2InputChoice then
+            if love.keyboard.wasPressed('down') then
+                sounds['beep']:play()
+                flashing = false
+                flashTimer = .5
+                letter2Index = letter2Index + 1
+                if letter2Index == 27 then
+                    letter2Index = 1
+                end
             end
-        end
-        if love.keyboard.wasPressed('up') then
-            sounds['beep']:play()
-            flashing = false
-            flashTimer = .5
-            letter2Index = letter2Index - 1
-            if letter2Index == 0 then
-                letter2Index = 26
+            if love.keyboard.wasPressed('up') then
+                sounds['beep']:play()
+                flashing = false
+                flashTimer = .5
+                letter2Index = letter2Index - 1
+                if letter2Index == 0 then
+                    letter2Index = 26
+                end
             end
-        end
-    elseif letter3InputChoice then
-        if love.keyboard.wasPressed('down') then
-            sounds['beep']:play()
-            flashing = false
-            flashTimer = .5
-            letter3Index = letter3Index + 1
-            if letter3Index == 27 then
-                letter3Index = 1
+        elseif letter3InputChoice then
+            if love.keyboard.wasPressed('down') then
+                sounds['beep']:play()
+                flashing = false
+                flashTimer = .5
+                letter3Index = letter3Index + 1
+                if letter3Index == 27 then
+                    letter3Index = 1
+                end
             end
-        end
-        if love.keyboard.wasPressed('up') then
-            sounds['beep']:play()
-            flashing = false
-            flashTimer = .5
-            letter3Index = letter3Index - 1
-            if letter3Index == 0 then
-                letter3Index = 26
+            if love.keyboard.wasPressed('up') then
+                sounds['beep']:play()
+                flashing = false
+                flashTimer = .5
+                letter3Index = letter3Index - 1
+                if letter3Index == 0 then
+                    letter3Index = 26
+                end
             end
         end
     end
@@ -170,35 +179,38 @@ function HighScoreState:render()
         love.graphics.printf(tostring(saveData[10].place ..  ' ' .. saveData[10].name[1] .. saveData[10].name[2] .. saveData[10].name[3]), 42, 120, VIRTUAL_WIDTH, 'left')
         love.graphics.printf('         ' .. tostring(saveData[10].score), -50, 120, VIRTUAL_WIDTH, 'right')
     end
-
-    if letter1InputChoice then
-        love.graphics.printf('A', 8, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
-        love.graphics.printf('A', 16, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
-        if flashing then
-            love.graphics.setColor(255/255, 255/255, 255/255, 0/255)
-        else
-            love.graphics.setColor(255/255, 255/255, 255/255, 255/255)
+---[[
+    if not playerInitialsLocked then
+        if letter1InputChoice then
+            love.graphics.printf('A', 8, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
+            love.graphics.printf('A', 16, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
+            if flashing then
+                love.graphics.setColor(255/255, 255/255, 255/255, 0/255)
+            else
+                love.graphics.setColor(255/255, 255/255, 255/255, 255/255)
+            end
+            love.graphics.printf(tostring(letters[letter1Index]), 0, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
+        elseif letter2InputChoice then
+            love.graphics.printf(tostring(scoreInitials[1]), 0, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
+            love.graphics.printf(tostring('A'), 16, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
+            if flashing then
+                love.graphics.setColor(255/255, 255/255, 255/255, 0/255)
+            else
+                love.graphics.setColor(255/255, 255/255, 255/255, 255/255)
+            end
+            love.graphics.printf(tostring(letters[letter2Index]), 8, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
+        elseif letter3InputChoice then
+            love.graphics.printf(tostring(scoreInitials[1]), 0, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
+            love.graphics.printf(tostring(scoreInitials[2]), 8, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
+            if flashing then
+                love.graphics.setColor(255/255, 255/255, 255/255, 0/255)
+            else
+                love.graphics.setColor(255/255, 255/255, 255/255, 255/255)
+            end
+            love.graphics.printf(tostring(letters[letter3Index]), 16, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
         end
-        love.graphics.printf(tostring(letters[letter1Index]), 0, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
-    elseif letter2InputChoice then
-        love.graphics.printf(tostring(scoreInitials[1]), 0, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
-        love.graphics.printf(tostring('A'), 16, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
-        if flashing then
-            love.graphics.setColor(255/255, 255/255, 255/255, 0/255)
-        else
-            love.graphics.setColor(255/255, 255/255, 255/255, 255/255)
-        end
-        love.graphics.printf(tostring(letters[letter2Index]), 8, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
-    elseif letter3InputChoice then
-        love.graphics.printf(tostring(scoreInitials[1]), 0, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
-        love.graphics.printf(tostring(scoreInitials[2]), 8, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
-        if flashing then
-            love.graphics.setColor(255/255, 255/255, 255/255, 0/255)
-        else
-            love.graphics.setColor(255/255, 255/255, 255/255, 255/255)
-        end
-        love.graphics.printf(tostring(letters[letter3Index]), 16, VIRTUAL_HEIGHT / 2, VIRTUAL_WIDTH, 'center')
     end
+    --]]
 
     if playerInitialsLocked then
         love.graphics.print('SCORE IS GREATER THAN SAVEDATA 10', 0, 200)
@@ -209,4 +221,5 @@ function HighScoreState:render()
        love.graphics.print('SCORE IS GREATER THAN SAVEDATA 10', 0, 200)
     end
     --]]
+--    love.graphics.print('playerInitialsLocked: ' .. tostring(playerInitialsLocked), 10, VIRTUAL_HEIGHT - 20)
 end
